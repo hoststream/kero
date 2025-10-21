3e542abc07aca1623580ab3d5631c72edc86173e
hoststream <hoststream1@hotmail.com>
2025-10-21 12:14:17 +0300

    Add files via upload

---
+++ manifest.php
@@ -0,0 +1,24 @@
+<?php
+header('Content-Type: application/json');
+echo json_encode([
+  "name" => "Kero App",
+  "short_name" => "Kero",
+  "start_url" => "/",
+  "display" => "standalone",
+  "background_color" => "#ffffff",
+  "theme_color" => "#000000",
+  "description" => "تطبيق كيرو - موقعك الرسمي.",
+  "icons" => [
+    [
+      "src" => "/icon-192.png",
+      "sizes" => "192x192",
+      "type" => "image/png"
+    ],
+    [
+      "src" => "/icon-512.png",
+      "sizes" => "512x512",
+      "type" => "image/png"
+    ]
+  ]
+], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+?>
